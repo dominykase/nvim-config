194e48c628328a10a661836aec14251ce6be6e74
dominykase <dominykas.eicinas@gmail.com>
2025-01-02 22:56:41 +0200

    updated key mapping

--- lua/dominykase/remap.lua
+++ lua/dominykase/remap.lua
@@ -2,5 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Explore)
 
 -- copy/paste to OS clipboard
-vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y`<gv')
 vim.keymap.set("n", "<leader>pt", '"+p')
+
+-- paste from OS clipboard in command mode (good for quickly grepping)
+vim.keymap.set("c", "<C-p>", "<C-r>+")
