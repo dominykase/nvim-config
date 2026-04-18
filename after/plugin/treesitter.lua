cafc03ba154202c5ff77080fff27a1a893398cb6
dominykas <dominykas.eicinas@gmail.com>
2026-04-18 23:33:50 +0300

    fix treesitter

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,24 +1,24 @@
-require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "php", "c", "lua", "vim", "vimdoc", "query", "go", "java", "ruby" },
+-- nvim-treesitter v2+ (full rewrite): `nvim-treesitter.configs` no longer exists.
+-- Parser installs need the `tree-sitter` CLI (0.26.1+) on PATH; see :h nvim-treesitter-quickstart.
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
-  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-  highlight = {
-    enable = true,
-
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+local langs = {
+  'javascript',
+  'typescript',
+  'php',
+  'c',
+  'lua',
+  'vim',
+  'vimdoc',
+  'query',
+  'go',
+  'java',
+  'ruby',
 }
+
+require('nvim-treesitter').install(langs)
+
+vim.api.nvim_create_autocmd('FileType', {
+  callback = function(args)
+    pcall(vim.treesitter.start, args.buf)
+  end,
+})
